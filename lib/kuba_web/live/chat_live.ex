2fd0bbb49b2738161d51f7cfe5301f35afdf9040
Jacob Atzen <jatzen@gmail.com>
2020-11-15 21:20:29 +0100

    Cleanup a bit

--- lib/kuba_web/live/chat_live.ex
+++ lib/kuba_web/live/chat_live.ex
@@ -31,20 +31,6 @@ defmodule KubaWeb.ChatLive do
     {:noreply, assign(socket, :chat, changeset(message))}
   end
 
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
-  end
-
   def handle_info({:speak, message}, socket) do
     IO.puts "#{inspect self()} received #{message}"
     new_socket = assign(socket, messages: messages)
@@ -63,26 +49,13 @@ defmodule KubaWeb.ChatLive do
   defp changeset() do
     data = %{message: ""}
     types = %{message: :string}
-    changeset = Ecto.Changeset.cast({data, types}, %{ message: ""}, [:message])
+    Ecto.Changeset.cast({data, types}, %{ message: ""}, [:message])
   end
 
   defp changeset(message) do
     data = %{message: ""}
     types = %{message: :string}
-    changeset = Ecto.Changeset.cast({data, types}, %{ message: message}, [:message])
-  end
-
-
-  defp search(query) do
-    if not KubaWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+    Ecto.Changeset.cast({data, types}, %{ message: message}, [:message])
   end
 end
 

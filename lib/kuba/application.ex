cd5337990b1992f2f776d6956f326b8ed8681dc3
Jacob Atzen <jatzen@gmail.com>
2021-01-05 21:33:36 +0100

    Start lobby on boot

--- lib/kuba/application.ex
+++ lib/kuba/application.ex
@@ -24,7 +24,10 @@ defmodule Kuba.Application do
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Kuba.Supervisor]
-    Supervisor.start_link(children, opts)
+    result = Supervisor.start_link(children, opts)
+
+    Kuba.Channels.start_channel("Lobby")
+    result
   end
 
   # Tell Phoenix to update the endpoint configuration

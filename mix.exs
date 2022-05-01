2b636b3e0323c6987bdd570faa9b84a36c7ca168
Jacob Atzen <jatzen@gmail.com>
2022-05-01 21:34:32 +0200

    Update tzdata

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule Kuba.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:tzdata, "~> 1.0.5"}
+      {:tzdata, "~> 1.1.1"}
     ]
   end
 
